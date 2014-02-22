0829e0efebfcd442cee4fea64523478be7b141f4
Pablo Neira Ayuso <pablo@gnumonks.org>
2014-02-22 22:19:35 +0100

    gtp-genl: fix compilation warning
    Fix these compilation warning:
    
    gtp-genl.c:131:8: warning: 'pdp.version' may be used uninitialized in this function [-Wmaybe-uninitialized]
    gtp-genl.c:132:8: warning: 'pdp.tid' may be used uninitialized in this function [-Wmaybe-uninitialized]
    d  CCLD   libgtpnl.la
    
    By initializing the pdp object.

--- src/gtp-genl.c
+++ src/gtp-genl.c
@@ -111,7 +111,7 @@ static int genl_gtp_validate_cb(const struct nlattr *attr, void *data)
 static int genl_gtp_attr_cb(const struct nlmsghdr *nlh, void *data)
 {
 	struct nlattr *tb[GTPA_MAX + 1] = {};
-	struct gtp_pdp pdp;
+	struct gtp_pdp pdp = {};
 	struct genlmsghdr *genl;
 
 	mnl_attr_parse(nlh, sizeof(*genl), genl_gtp_validate_cb, tb);
