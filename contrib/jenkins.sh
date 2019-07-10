d160a73a4134b5ad95b80c44c73dbcf52dffb5d7
Oliver Smith <osmith@sysmocom.de>
2019-07-10 11:57:27 +0200

    contrib/jenkins.sh: run "make maintainer-clean"
    Related: OS#3047
    Change-Id: I20997b986b24ced9bb8d6d66a66d5ec2bafd00f2

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -8,5 +8,6 @@ autoreconf --install --force
 ./configure --enable-sanitize CFLAGS="-Werror" CPPFLAGS="-Werror"
 $MAKE $PARALLEL_MAKE
 $MAKE distcheck
+$MAKE maintainer-clean
 
 osmo-clean-workspace.sh
